83f995f8659f19feb1a897f6fdf8e9a02c26b4fe
SongAlbum <115658647+SongAlbum@users.noreply.github.com>
2025-05-14 15:01:55 +0700

    Update List.js

--- List.js
+++ List.js
@@ -1,5 +1,6 @@
 const albumTracks = [
             {
+                nomor: 1,
                 title: "Ungu - Andai Ku Tahu",
                 artist: "Ungu",
                 src: "https://github.com/MusicEnjoy/cek-sound/raw/main/DJ%20GALA-GALA%20BY%20R2%20PROJECT%20SLOW%20BASS.%20NDEWOR%20AUDIO.mp3",
@@ -7,6 +8,7 @@ const albumTracks = [
                 cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg1VeQ8DjVi9HYl-BqYIK9IV2yWbemvezkLxIdcxoT7fEaHgat18T-nKn4qzXIZ2dPNZO6mZEnAMcTMWMJ6fWcyoIWAqtkeOQZktemBYtF-L9NG1c16aqQ8do5rNkQ-xD8SJuJdohHTmPh3Ok7mMV-6BqUqDKmyW3KpLuCS-7Edq_D0AtOA8ZqloSn8d6A/s320/AB.png",
             },
             {
+                nomor: 2,
                 title: "Ungu - Demi Waktu",
                 artist: "Ungu",
                 src: "https://github.com/MusicEnjoy/Random/raw/main/TIARA%20-%20KRIS%203PB%20SALLSA%20BINTAN.mp3",
@@ -14,6 +16,7 @@ const albumTracks = [
                 cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg1VeQ8DjVi9HYl-BqYIK9IV2yWbemvezkLxIdcxoT7fEaHgat18T-nKn4qzXIZ2dPNZO6mZEnAMcTMWMJ6fWcyoIWAqtkeOQZktemBYtF-L9NG1c16aqQ8do5rNkQ-xD8SJuJdohHTmPh3Ok7mMV-6BqUqDKmyW3KpLuCS-7Edq_D0AtOA8ZqloSn8d6A/s320/AB.png",
             },
             {
+                nomor: 3,
                 title: "RUNTAH Remix",
                 artist: "AZMY Z",
                 src: "https://github.com/MusicEnjoy/Random/raw/main/RUNTAH%20REMIX%20BY%20AZMY%20Z%20FT%20IMP%20ID%20%23azmyz%20%23runtah%20%23lagusunda.mp3",
